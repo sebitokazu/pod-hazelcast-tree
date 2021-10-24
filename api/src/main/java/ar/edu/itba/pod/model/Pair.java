f60d78aba21014e97db18b82c871d2c3ef6e754e
Seba Itokazu <sitokazu@itba.edu.ar>
2021-10-23 21:04:08 -0300

    Query2 MapReduce

---
+++ api/src/main/java/ar/edu/itba/pod/model/Pair.java
@@ -0,0 +1,35 @@
+package ar.edu.itba.pod.model;
+
+import java.util.Objects;
+
+public class Pair<K,V> {
+
+    private K left;
+    private V right;
+
+    public Pair(K left, V right){
+        this.left = left;
+        this.right = right;
+    }
+
+    public K getLeft() {
+        return left;
+    }
+
+    public V getRight() {
+        return right;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        return getLeft().equals(pair.getLeft()) && getRight().equals(pair.getRight());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getLeft(), getRight());
+    }
+}
